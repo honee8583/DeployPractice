26219a82a5b1bfc5c1386ef3031eafb5f1834986
honee8583 <honee8583@naver.com>
2024-02-08 02:44:20 +0900

    feat: Hello API 생성 및 테스트 코드 작성
    - SpringBoot 2.x버전 사용.
    - JUnit4 사용.
    - build.gradle -> jcenter 추가.

---
+++ src/test/java/com/practice/deploypractice/web/HelloControllerTest.java
@@ -0,0 +1,45 @@
+package com.practice.deploypractice.web;
+
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+// 테스트를 진행할 때 JUnit에 내장된 실행자 외에 다른 실행자를 실행.(여기서는 SpringRunner 실행자를 실행)
+// 스프링부트 테스트와 JUnit 사이에 연결자 역할을 한다.
+@RunWith(SpringRunner.class)
+// Web(Spring MVC)에 집중할 수 있는 어노테이션
+// (선언할 경우 @Controller, @ControllerAdvice등을 사용할 수 있다.
+// 단, @Service, @Component, @Repository 등은 사용불가.) 여기서는 컨트롤러만 사용하기 때문에 사용.
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+
+    // 웹 API를 테스트할 때 사용.
+    // 스프링 MVC 테스트의 시작점.
+    // 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트 가능.
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        // MockMvc를 통해 /hello 주소로 HTTP GET 요청 수행.
+        // 여러 검증을 이어서 선언 가능.
+        mvc.perform(get("/hello"))
+                // mvc.perform의 결과를 검증.
+                // HTTP Header의 Status를 검증. (200, 400, 500 등의 상태를 검증.)
+                .andExpect(status().isOk())
+                // mvc.perform의 결과를 검증.
+                // 응답 본문의 내용을 검증.
+                // Controller에서 "hello"를 리턴하기 때문에 이 값이 맞는지 검증.
+                .andExpect(content().string(hello));
+    }
+}
