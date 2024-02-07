f85e84c84623d74062b5bb5a30e4143ba34154e0
honee8583 <honee8583@naver.com>
2024-02-08 02:18:28 +0900

    feat: initial commit

---
+++ src/test/java/com/practice/deploypractice/DeployPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.deploypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
